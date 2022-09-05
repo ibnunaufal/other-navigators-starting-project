39e4458600f01a62896c54cc7328961e62999844
Ibnu Naufal <ibnunaufal5758@gmail.com>
2022-09-05 16:37:33 +0700

    setup drawer & bottom tab

--- App.js
+++ App.js
@@ -1,5 +1,24 @@
-import { Text } from 'react-native';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar, Text } from "react-native";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
-  return <Text>Todo...</Text>;
+  return (
+    <NavigationContainer>
+      {/* <Drawer.Navigator initialRouteName="User">
+        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+        <Drawer.Screen name="User" component={UserScreen} />
+      </Drawer.Navigator> */}
+      <BottomTab.Navigator initialRouteName="User">
+        <BottomTab.Screen name="Welcome" component={WelcomeScreen} />
+        <BottomTab.Screen name="User" component={UserScreen} />
+      </BottomTab.Navigator>
+    </NavigationContainer>
+  );
 }
